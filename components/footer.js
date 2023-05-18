43e3943cbce2a42def7524f95a5b657fc009273f
Mariano Villaescusa <msvillaescusa@gmail.com>
2023-05-18 19:36:24 -0300

    update 18/05

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 import Link from "next/link"
 
 const Footer = () => (
-  <div className="container" style={{paddingLeft: "60%"}}>
+  <div className="container" style={{paddingLeft: "60%", marginBottom:"10px"}}>
     <Link href="https://www.linkedin.com/in/mariano-salvador-villaescusa-11364aaa/">
       <img src="linkedin.png" alt="linkedin" width={"50px"} style={{borderRadius:"100%"}}/>
     </Link>
