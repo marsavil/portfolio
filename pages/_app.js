9c700b5a2cb8a088867c76de3f195593a8ccd52c
Mariano Villaescusa <msvillaescusa@gmail.com>
2023-05-19 11:13:59 -0300

    update 19/05

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import 'bootswatch/dist/cyborg/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { useEffect } from 'react';
 
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap.bundle.min.js');
+  }, [])
   return <Component {...pageProps} />;
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,19 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <title>My Portfolio</title>
-        <meta name="description" content="Mariano Villaescusa Portfolio Website"/>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>My Portfolio</title>
+          <meta name="description" content="Mariano Villaescusa Portfolio Website" />
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
