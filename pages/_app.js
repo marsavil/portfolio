c076cfe82cb12b1eb06310cc1fd55401e7ff904e
Mariano Villaescusa <msvillaescusa@gmail.com>
2023-05-19 11:36:00 -0300

    updated 19/05

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
-import 'bootswatch/dist/cyborg/bootstrap.min.css'
+import 'bootswatch/dist/cyborg/bootstrap.min.css';
 import { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 
 
 export default function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    
-  }, [])
+    if (typeof window !== 'undefined') {
+      import('bootstrap/dist/js/bootstrap.bundle.min.js');
+    }
+  }, []);
   return (
 <Container>
   <Component {...pageProps} />
