dd8294c01e14276626e59f5e70f3094f7ad492e6
Mariano Villaescusa <msvillaescusa@gmail.com>
2023-07-11 18:43:57 -0300

    update experience

--- data/profile.js
+++ data/profile.js
@@ -42,5 +42,11 @@ export const experience = [
   description: "700-hour course, which included theoretical and practical classes, focusing on JavaScript, HTML, and CSS. The course also involved the development of projects using React, incorporating Redux on the front-end, as well as building RESTful APIs with Node.js using Express and PostgreSQL. Both individual and group projects were completed during the course.",
   from:"Sept 2022",
   to: "May 2023"
+},
+{
+  institution: "Freelance",
+  description: "Development of a Backend designed to manage users and created on javascript and node js through express and sequelze .",
+  from:"June 223",
+  to: "July 2023"
 }
 ]
