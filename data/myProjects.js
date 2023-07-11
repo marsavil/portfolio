9272f3ccc1ba3f1d409abc392343486ad1705c85
Mariano Villaescusa <msvillaescusa@gmail.com>
2023-07-11 18:32:02 -0300

    agregado manager test

--- data/myProjects.js
+++ data/myProjects.js
@@ -10,7 +10,14 @@ const projects = [
     description: "Academic experience.\nHenry Bootcamp, Buenos Aires, Argentina. \n Designed and developed an e-commerce App that included: authentication, searches, combined filters,\nsorting, user banning, email sending, etc.\nFront: Cloudinary, React-Google-Login, Mercado Pago\nBack: Express, NodeJs, JasonWebToken, Bcrypt, Nodemailer, Mercado Pago. Development of routes and requests\nDatabase: PostgreSQL and Sequelize.",
     image: "https://res.cloudinary.com/dlzp43wz9/image/upload/v1684356166/electroshop_ln4jpv.png",
     repository: "https://github.com/marsavil/Electroshop"
+  },
+  {
+    name: "Manager Test",
+    description: "Professional experience. \nFreelancer \nBackend designed to manage users - Admin, Managers, Referrals and Afiliates. Includes a relational database built with postgres, referral registration, affiliate users registration from the front end or via admin/manager action, logs in valid for X minutes  and creation of sales logs. \n Built with Express, NodeJs, JasonWebToken, Bcrypt, Nodemailer\nDatabase: PostgreSQL and Sequelize.",
+    image: "https://res.cloudinary.com/dlzp43wz9/image/upload/v1689111055/codemanager_orwqrq.png",
+    repository: "https://github.com/marsavil/manager"
   }
 ]
+
 
 export default projects
